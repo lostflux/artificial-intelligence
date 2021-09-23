8e15bc3d2afa98c54b32c9db4d682c104aab9d94
Amittai Siavava <amittaijoel@outlook.com>
2021-09-23 14:37:15 -0400

    working BFS

--- PA1/FoxProblem.py
+++ PA1/FoxProblem.py
@@ -3,8 +3,8 @@ class FoxProblem:
         self.start_state = start_state
         self.goal_state = (0, 0, 0)
         self.state = start_state
-        self.chickens = start_state[0]
-        self.foxes = start_state[1]
+        self.max_chickens = start_state[0]
+        self.max_foxes = start_state[1]
         self.boat_ashore = bool(start_state[2])
 
         # you might want to add other things to the problem,
@@ -18,35 +18,66 @@ class FoxProblem:
         next_states = set()
         for num_chickens in range(0, 3):
             for num_foxes in range(0, 3):
-                if 0 < (num_chickens + num_foxes) <= 2:                  # ignore states the empty transition
-                    new_chickens = self.chickens - num_chickens         # find new number of chicken ashore
-                    new_foxes = self.foxes - num_foxes                  # find new number of foxes ashore
-                    boat_status = not self.boat_ashore                  # flip boat status
-                    state = (new_chickens, new_foxes, int(boat_status))
+                if 0 < (num_chickens + num_foxes) <= 2:  # ignore states the empty transition
+                    if bool(state[2]):
 
-                    # check the state for validity, append to valid states
-                    if self.is_valid(state):
-                        next_states.add(state)
+                        # BOAT LEAVING SHORE
+                        new_chickens = state[0] - num_chickens  # find new number of chicken ashore
+                        new_foxes = state[1] - num_foxes  # find new number of foxes ashore
+                        boat_ashore = not bool(state[2])  # flip boat status
+                        next_state = (new_chickens, new_foxes, int(boat_ashore))
 
+                        # check the state for validity, append to valid states
+                        if self.is_valid(next_state):
+                            next_states.add(next_state)
+                    else:
+
+                        # BOAT RETURNING TO SHORE
+                        new_chickens = state[0] + num_chickens  # find new number of chicken ashore
+                        new_foxes = state[1] + num_foxes  # find new number of foxes ashore
+                        boat_ashore = not bool(state[2])  # flip boat status
+                        next_state = (new_chickens, new_foxes, int(boat_ashore))
+
+                        # check the state for validity, append to valid states
+                        if self.is_valid(next_state):
+                            next_states.add(next_state)
         # return set of states
         return next_states
 
     # I also had a goal test method. You should write one.
-    def goal_state(self, state):
+    def is_goal(self, state):
         return self.goal_state == state
 
-    @staticmethod
-    def is_valid(state):
-        return state[0] >= state[1]
+    def is_valid(self, state):
+
+        if state[0] <= self.max_chickens and state[1] <= self.max_foxes:
+            return 0 <= state[1] <= state[0]
+
+        return False
 
     def __str__(self):
-        string =  "Chickens and foxes problem: " + str(self.start_state)
+        string = "Chickens and foxes problem: " + str(self.start_state)
         return string
 
 
 ## A bit of test code
 
+def check_states(state, goal_state):
+    if state == goal_state:
+        print("Found!")
+        return
+
+    print(test_cp.get_successors(state))
+    for n_state in test_cp.get_successors(state):
+        check_states(n_state, goal_state)
+
+
 if __name__ == "__main__":
     test_cp = FoxProblem((5, 5, 1))
     print(test_cp.get_successors((5, 5, 1)))
+    start_state = (5, 5, 1)
+    goal_state = (0, 0, 0)
+    check_states(start_state, goal_state)
+    # while True:
+
     print(test_cp)
--- PA1/uninformed_search.py
+++ PA1/uninformed_search.py
@@ -1,7 +1,7 @@
-
 from collections import deque
 from SearchSolution import SearchSolution
 from FoxProblem import *
+
 
 # you might find a SearchNode class useful to wrap state objects,
 #  keep track of current depth for the dfs, and point to parent nodes
@@ -14,17 +14,42 @@ class SearchNode:
         self.state = state
         self.parent = parent
 
+
 # you might write other helper functions, too. For example,
 #  I like to separate out backchaining, and the dfs path checking functions
 
 
 def bfs_search(search_problem: FoxProblem):
-
     solution = SearchSolution(search_problem, "BFS")
 
-    visit_queue = deque()
     start_node = SearchNode(search_problem.state)
+    visit_queue = deque()
     visit_queue.append(start_node)
+
+    visited_states = set()
+
+    final_node = None
+
+    while len(visit_queue) > 0:
+        current_node = visit_queue.popleft()
+        if not search_problem.is_goal(current_node.state):
+            for next_state in search_problem.get_successors(current_node.state):
+                if next_state not in visited_states:
+                    new_node = SearchNode(next_state, current_node)
+                    visit_queue.append(new_node)
+                    visited_states.add(next_state)
+        else:
+            final_node = current_node
+            break
+
+    if final_node is None:
+        solution.path = "No path was found."
+    else:
+        while final_node is not None:
+            solution.path.insert(0, final_node.state)
+            final_node = final_node.parent
+
+    return solution.path
 
 
 # Don't forget that your dfs function should be recursive and do path checking,
@@ -40,7 +65,9 @@ def dfs_search(search_problem, depth_limit=100, node=None, solution=None):
         solution = SearchSolution(search_problem, "DFS")
 
     # you write this part
-
+    current_state = search_problem.start_state
+    for next_state in search_problem.get_successors(current_state):
+        dfs_search(search_problem, next_state, node)
 
 
 def ids_search(search_problem, depth_limit=100):
