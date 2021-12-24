57f5829b7d245b41fd84915cdb2c01f2b0efeeeb
siavava <amittaijoel@outlook.com>
2021-12-23 20:02:48 -0500

    Started Fibonacci Tree?

---
+++ data_structures/fibonacci_tree.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    A naive Fibonacci Tree.
+"""
+__author__ = "Amittai"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Amittai"]
+__email__ = "Amittai.J.Wekesa.24@dartmouth.edu"
+__github__ = "@siavava"
